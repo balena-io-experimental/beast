013b7079502ef504fa9c0bd8e266ef83ba8c8ef1
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-08-15 18:46:26 +0100

    Added default rotate parameter

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "rendering image"
+echo "Rendering image"
+
+if [ -z "$ROTATE" ]; then
+    ROTATE=0
+fi
 
 convert images/image.png -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
