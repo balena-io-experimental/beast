3ebacd5f933df92f5c85bb44993c0f740c54c52e
Ronald McCollam <mccollam@gmail.com>
2016-08-30 10:42:33 -0400

    Parameterize image filename

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+IMAGE="image.png"
+
 echo "Rendering image"
 
 if [ -z "$ROTATE" ]; then
     ROTATE=0
 fi
 
-convert images/image.png -rotate $ROTATE images/rotated.png
+convert images/"$IMAGE" -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
 while true; do
