5c029ff54ee8da9da93d491dda52e7efab293f0a
Ronald McCollam <mccollam@gmail.com>
2016-09-07 05:56:44 -0400

    Allow setting of image as Resin environment variable
    * Parameterize image filename
    
    * Allow setting of image as Resin environment variable (thanks Page-!)
    
    * Use cleaner defaulting for ROTATE variable

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+IMAGE=${IMAGE:-"image.png"}
+
 echo "Rendering image"
 
-if [ -z "$ROTATE" ]; then
-    ROTATE=0
-fi
+ROTATE=${ROTATE:-0}
 
-convert images/image.png -rotate $ROTATE images/rotated.png
+convert images/"$IMAGE" -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
 while true; do
