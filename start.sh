fc6740426d0baf5e300161d7eb00d34b063725b3
Ronald McCollam <mccollam@gmail.com>
2016-10-24 12:55:05 -0400

    m

--- start.sh
+++ start.sh
@@ -9,6 +9,4 @@ ROTATE=${ROTATE:-0}
 convert images/"$IMAGE" -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
-while true; do
-    sleep 1
-done
+sleep infinity
