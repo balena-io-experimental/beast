41c535a3b99ea552204f4d206ac3905bb719fe76
Ronald McCollam <mccollam@gmail.com>
2017-09-12 17:05:08 +0200

    m

--- start.sh
+++ start.sh
@@ -6,11 +6,6 @@ echo "Rendering image"
 
 ROTATE=${ROTATE:-0}
 
-echo "Doing something awful..."
-sleep 10
-echo "OH NO!"
-exit 1
-
 convert images/"$IMAGE" -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
