da09ff37d692aad9a15b5f7e5aeb0690d30d7e4a
Andrew Lucas <sqweelygig@googlemail.com>
2017-09-19 14:57:25 +0300

    wip

--- examples/clock.js
+++ examples/clock.js
@@ -59,7 +59,7 @@ var update = function() {
     fb.color(1, 1, 1);
     fb.circle(xMax/2, yMax/2, radius * 0.85);
     try {
-        fb.image(xMax/2 - 16, yMax/2 + radius * 0.50 - 16, "examples/mage.png");
+        fb.image(xMax/2 - 16, yMax/2 + radius * 0.50 - 16, "examples/image.png");
     }
     catch(error) {
         console.log(error);
