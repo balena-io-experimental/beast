9e83cfe81e025feb2ce07eec30928c45ab8c17d3
Shaun Mulligan <switchflip.shaun@gmail.com>
2017-10-16 11:43:54 +0200

    Allow resizing via env vars

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-IMAGE=${IMAGE:-"image.png"}
+IMAGE=${IMAGE:-"moby.png"}
 
 echo "Rendering image"
 
 ROTATE=${ROTATE:-0}
+if [ -z ${RESIZE+x} ]; then RESIZE_STR=""; else RESIZE_STR="-resize ${RESIZE}!"; fi
 
-convert images/"$IMAGE" -rotate $ROTATE images/rotated.png
+convert images/"$IMAGE" -rotate $ROTATE ${RESIZE_STR} images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
 while true; do
